9b953507659e5fcffb0034a5a2dc432fe77c01c0
QuadFlame <jeremymaunder@icloud.com>
2022-05-07 18:49:50 +1200

    V1

--- src/main/java/com/quadflame/blockdecay/Events/EventBlockDecay.java
+++ src/main/java/com/quadflame/blockdecay/Events/EventBlockDecay.java
@@ -20,11 +20,6 @@ public class EventBlockDecay implements Listener {
     @EventHandler
     public void onBlockPlace(BlockPlaceEvent e){
         Player p = e.getPlayer();
-        p.getInventory().setItem(1, new ItemStack(Material.SANDSTONE, 64));
-        p.getInventory().setItem(2, new ItemStack(Material.SANDSTONE, 64));
-        if(p.getLocation().getY() >= 99){
-            e.setCancelled(true);
-        }
         Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable(){
             @Override
             public void run(){
